77c238f3e5dede6bc879dca9b89d08fe06e25748
adityameshram-dev <adityabmeshram358@gmail.com>
2026-04-07 01:14:11 +0530

    List program added

---
+++ list.py
@@ -0,0 +1,28 @@
+#Python Lists
+
+mylist=["apple","banana","cat","dog","elephant"]
+
+print(mylist)      # print values from list 
+
+print(len(mylist)) # print how many values in list
+
+print(mylist[0])    # it print 1st element of list
+
+# we can perform more operation like membership etc     
+if 'apple' in mylist:
+    print("apple word present in the list")
+else:
+    print("Apple word not present i list")
+
+
+# List is data structure in python
+# List are ordered in python
+# List are mutable in python
+# List allow to store duplicate value
+# List allow to store heterogeneous data
+# List data accesssed by their index
+# List 1st item has 0 index
+# List allow negative index eg. list[-1] means last item
+# Lists are dynamic and growable
+# Lists are iterable
+# Lists support nesting (lists inside lists).
