d59dd6cd46ff69ad1b6d1f91c13fdc88796112ee
adityameshram-dev <adityabmeshram358@gmail.com>
2026-04-07 01:19:49 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,29 @@
+# Python Set
+
+thiset = {"bittu", "aditya", "meshram", "elephent"}
+myset = {"apple", "banana", "cat", "dog"}
+
+print("myset: ",myset)   # it print values i.e store in myset 
+
+for i in myset:
+    print(i)    # access the set value using loop
+
+
+# Set is data structure in Python.
+# Sets are unordered in Python.
+# Sets are mutable in Python.
+# Sets not allow to store duplicate value.
+# Sets allow to store heterogeneous data.
+# Set data is not accessed by their index.
+# Sets are dynamic and growable.
+# Sets are iterable.
+# Sets support nesting (sets inside sets) note: Possible using only frozenset
+# Set elements must be hashable (immutable types).
+# Sets support set operations (union, intersection, difference).
+
+# note
+'''
+Set items are unchangeable (immutable)
+but
+The set itself is changeable (mutable)
+'''
---
+++ tuple.py
@@ -0,0 +1,28 @@
+#Python Tuple
+
+tp = (1,2,3,4,5,)
+
+print(tp)        # print values from list    
+
+print(len(tp))   #  print how many values in list
+
+print(tp[0])     # first item has index 0 up to n-1 
+
+
+# we can perform more operation like membership etc     
+if 5 in tp:
+    print("5 present in the list")
+else:
+    print("5 not present i list")
+
+
+# Tuple is data structure in python
+# Tuple are ordered in python
+# Tuple are immutable in python
+# Tuple allow to store duplicate value
+# Tuple allow to store heterogeneous data
+# Tuple data accesssed by their index
+# Tuple 1st item has 0 index
+# Tuple allow negative index eg. list[-1] means last item
+# Tuples are hashable (can be dictionary keys)
+# Tuples have fixed size (cannot add/remove items)
