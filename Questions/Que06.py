4bbde3ad4e8bc712da59d35872876f31ba4ecf42
adityameshram-dev <adityabmeshram358@gmail.com>
2026-04-12 22:54:33 +0530

    Fix leap year condition in Que06.py

--- Questions/Que06.py
+++ Questions/Que06.py
@@ -2,7 +2,7 @@
 
 yr = int(input("Enter year: "))
 
-if yr%4==0 and yr%100!=00 or yr%400==0:
+if (yr%4==0 and yr%100!=0) or yr%400==0:
     print(f"{yr} is leap year")
 else:
     print(f"{yr} is not leap year")
